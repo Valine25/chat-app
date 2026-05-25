7fb7a17bf212a7d88342de80ce40ed618147d15c
Valine Noronha <valinenoronha25@gmail.com>
2026-05-25 20:10:47 +0530

    Backend removed

--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const { signup, login, logout } = require("../controllers/auth.controller.js");
-const authRoutes=express.Router();
-
-authRoutes.post("/signup",signup)
-authRoutes.post("/login",login)
-authRoutes.post("/logout",logout)
-
-exports.authRoutes=authRoutes;
